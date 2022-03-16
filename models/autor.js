ef8eb4a453c90f0d07164e20cb182bcafc99bdaf
MariaCarol <86033398+EstudanteMariaCarol@users.noreply.github.com>
2022-03-16 20:41:58 +0000

    fiz o atualizar/update

--- models/autor.js
+++ models/autor.js
@@ -7,14 +7,19 @@ class Autor{
      const values = [data.nome, data.sobrenome, data.data_nascimento]
      return await connect.query(sql,values);
   }
+  
   static async selecionar(){
     const connect = await db.connect();
     return await connect.query("select * from autores");
   }
+  
   static async atualizar(){
-     const connect = await db.connect();
-    return await connect.query("update * from autores");
+    const connect = await db.connect();
+    const sql = "update autores set nome=$1, sobrenome=$2, data_nascimento=$3, WHERE id=$4";
+    const values = [data.nome, data.sobrenome, data.data_nascimento, id]
+    return await connect.query(sql,values);
   }
+  
   static async deletar(){
      const connect = await db.connect();
     return await connect.query("delect * from autores");
